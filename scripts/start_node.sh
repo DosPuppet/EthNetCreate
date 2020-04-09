eddb1da10d390b725fc73f458f6400820ee99a10
Bastien <bvoituriez@gmail.com>
2020-04-09 14:43:05 +0200

    Update start_node.sh

--- scripts/start_node.sh
+++ scripts/start_node.sh
@@ -1,1 +1,1 @@
-parity --config node.toml --min-gas-price=0 --gas-price-percentile=0 --price-update-period="365000 days" --tracing=on --fat-db=on --pruning=archive --no-persistent-txqueue --force-sealing --jsonrpc-cors='*' --jsonrpc-interface="0.0.0.0" --ws-interface="0.0.0.0" --ws-origins='*'
+parity --config node.toml --min-gas-price=0 --gas-price-percentile=0 --price-update-period="365000 days" --tracing=on --fat-db=on --pruning=archive --no-persistent-txqueue --jsonrpc-cors='*' --jsonrpc-interface="0.0.0.0" --ws-interface="0.0.0.0" --ws-origins='*'
